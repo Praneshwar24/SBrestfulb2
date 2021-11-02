a917c015c7389ec782c1ae14e6f6a56ebdee18bf
Lenovo <Lenovo@LenovoE4125>
2021-11-02 14:01:45 +0530

    Student pojo student service -- returning hardcoded students

--- SBrestb2/src/main/java/dxc/b2/rest/StudentController.java
+++ SBrestb2/src/main/java/dxc/b2/rest/StudentController.java
@@ -1,6 +1,10 @@
 package dxc.b2.rest;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -11,6 +15,14 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class StudentController {
+	@Autowired
+	StudentService studentService;
+
+	@RequestMapping("/students")
+	public List<Student> getStudents(){
+		return studentService.getStudents();
+	}
+
 
 	@GetMapping("/")
 	public String home() {
---
+++ SBrestb2/src/main/java/dxc/b2/rest/StudentService.java
@@ -0,0 +1,31 @@
+package dxc.b2.rest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * this class will query the database and get the students required
+ * @author Lenovo
+ *
+ */
+
+@Service
+public class StudentService {
+
+	List<Student> students = new ArrayList<>();
+
+	public void createStudents() {
+		students.add(new Student(1, "Naruto", 1, 11));
+		students.add(new Student(2, "Sasuke", 2, 22));
+		students.add(new Student(3, "Sakura",3, 33));
+		students.add(new Student(4, "Kakashi", 4, 44));
+
+	}
+	public List<Student> getStudents(){
+		createStudents();
+		return students;
+	}
+
+}
